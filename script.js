c4e8e85cc0d0adcfa7178933e0962ed0c74e9f44
Millicent Aku Donkor <millicentaku23@gmail.com>
2024-11-09 13:59:38 +0000

    My first JS project

---
+++ script.js
@@ -0,0 +1,374 @@
+// console.log("hello Osika,welcome!");
+
+// var catAge =5;
+// let catName ="Rick"
+// const isEmpty = false;
+// let catNewAge = null;
+// let catInfo = {ageCat: 5, nameCat:"Rick", emptyIs:true};
+// let myInfo ={nameCat:"osika",ageCat:25, statusCat:"single" ,complexion:"chocolate" ,phoneCat:"245043099"}
+// let evenNumbers =[2,4,6,8,10,{odd1:3, odd2:5, odd3:7}];
+// catAge=10
+// console.log(myInfo.phoneCat)
+// console.log(catAge);
+// console.log(catName);
+// console.log(isEmpty);
+
+// console.log("The name of my cat is" + catName + "he is" + catAge + "old")
+
+// console.log(catName);
+// console.log(isEmpty);
+// console.log(catNewAge);
+// console.log(catInfo)
+// console.log(evenNumbers[2]);
+// console.log(evenNumbers[4]);
+// console.log(evenNumbers[5].odd2)
+
+
+// let numbers ={
+//               evenNumbers:[2,4,6,8,10],
+//               oddNumbers:{odd1:3, odd2:5, odd3:7}
+// };
+// console.log(numbers.evenNumbers[3]);
+// console.log(numbers.oddNumbers.odd2);
+
+// // operators
+// // addition(+)
+
+// let firstNumber=20;
+// let secondNumber=30;
+// let result= firstNumber+ secondNumber
+// console.log(result)
+
+
+// let numberGroup={firstNumber:20,
+//                 secondNumber:30}
+
+//  let finalObjResult = numberGroup.firstNumber + numberGroup.secondNumber
+//  console.log(finalObjResult);
+ 
+//  console.log(100+50);
+
+//  //subtraction
+//  let subNumber1 = 40;
+//  let subNumber2 = 30;
+//  let subResult = subNumber1 - subNumber2
+//  console.log(subResult)
+
+
+//  //multiplication
+//  let multiNumber1 = 40
+//  let multiNumber2 = 30
+//  let multiResult = multiNumber1*multiNumber2
+//  console.log(multiResult)
+
+//  //remainder operation(%)
+//  let modNumber = 30;
+//  let remainder = modNumber%4
+//  console.log(remainder);
+
+//  let anyNumber = 20
+//  const modConst = 2
+//  console.log(anyNumber%modConst)
+
+//  //logical operators
+ 
+//  //AND(&&)
+
+//  let isRaining = true;
+//  let isPresent = true;
+//  isRaining = false;
+//  isPresent = false;
+//  let finalResult = isRaining && isPresent;
+
+
+//  console .log (finalResult)
+
+//  //OR(||)
+
+//  let isCompleted = true;
+//  let isFull = false;
+//  let orResult = isCompleted || isFull
+//  console.log(orResult)
+
+//  //NOT
+
+//  let isDone = true;
+//  console.log(!isDone)
+
+//  console.log(!((isCompleted || isDone) && (isFull && isPresent)))
+
+//  //exponential operation
+
+//  let baseNumber = 10;
+//  let exponent = 5;
+//  let exponentialResult =(baseNumber**exponent)
+//  console.log(exponentialResult);
+
+//  //comparison operation
+//  let compNumber1 = 10;
+//  let compNumber2 = 5;
+//  compNumber2 = "10"; 
+
+
+//  let comparisionResult =(compNumber1 === compNumber2);
+
+//  console.log(comparisionResult);
+
+//  let firstWord = "boys";
+//  let secondWord = "girl";
+//  secondWord = "BOYS";
+//  let comparisionWord = firstWord === secondWord;
+//  console.log (comparisionWord)
+
+//  //controlflow
+
+//  let amount = "100";
+//  let priceOfBread = 20;
+//  let allowedToCut = true;
+
+//  if(amount >= priceOfBread){
+//     console.log("Buy Bread");
+//  }
+//  else if(amount < priceOfBread){
+//     console.log("buy biscuite")
+//  }
+// console.log("hi, hello");
+
+
+// let studentMarks = 90;
+
+// if(studentMarks >= 80 && studentMarks >= 100){
+//     console.log("A");
+// }
+// else if(studentMarks <= 79 && studentMarks >= 70){
+//     console.log("B");
+// }
+// else if(studentMarks <= 79 && studentMarks >= 60){
+//     console.log("C");
+// }
+// else{
+//     console.log("F") 
+// }
+
+// let breaadPrice = 100
+
+// for(let i = breaadPrice; i >= 20; i =i-20){
+//     console.log("buy bread")
+
+// }
+
+// let keyPrice = 1000
+// for(let i = 1; i <= 100; i ++){
+//     console.log(i)
+// }
+
+// //while loop
+// let j = 0;
+// while(j <= 100){
+//     console.log(j);
+//     j++;
+// }
+
+// let newArray = ["boy" , "cat", 1, 2, true ];
+// let newVariable = 0
+// while(newVariable<5){
+
+//     console.log(newArray[newVariable]);
+//     newVariable++
+
+// }
+// let mixedNumbers=[1,2,8,9,10,11,16,14]
+// let y =0
+// while (y<=7) {
+//     if (mixedNumbers[y]%2===0){
+//         console.log(mixedNumbers[y]);
+//     }y++}
+
+// function addNumbers(x,y){
+//     let sum = x+y;
+//     return sum ;
+// }
+// console.log(addNumbers(40,10))
+
+
+// function areaOfRectangle(length, width){
+//     let areaOfRectangle = length + width;
+//     return areaOfRectangle;
+// }
+// console.log(areaOfRectangle(24,30))
+
+
+// console.log(areaOfRectangle(10,29))
+
+// //function
+// function sum(){
+//     let a = 23;
+//     let b = 24;
+//     let result = a + b;
+//     return result
+// }
+// console.log(sum())
+
+// //method or function
+// let randomNumber = '40.9';
+// console.log(parseFloat(randomNumber))
+
+// console.log(isFinite(randomNumber));
+
+// console.log(parseInt(randomNumber));
+
+// console.log(Math.floor(randomNumber));
+
+// console.log(Math.floor(randomNumber));
+
+// randomNumber = Math.round(Math.random(10));
+
+// console.log(randomNumber);
+
+// let words = 'the cat is in the box';
+
+// console.log(words.slice(5))
+// console.log(words.split(" "))
+
+// console.log(words.charAt(5))
+
+// console.log(mixedNumbers.length);
+// console.log(mixedNumbers.pop(7))
+
+// mixedNumbers.push(15)
+// console.log(mixedNumbers)
+
+// alert("hello am bere to solve your problem");
+// let userInput = prompt("enter a value here")
+
+
+
+
+// function areaOfAsquare(l){return l**2}
+
+//     function perimeterOfAsquare(l,w){return 4*l};
+//      function areaOfRectangle(l,w){return l*w};
+// function perimeterOfRectangle(l,w){return 2*(l+w);}
+
+
+
+
+     
+// let userInput = prompt("choose a shape. 1. square 2. rectangle");
+// if(userInput === "1"){
+//     let squareuseroption = prompt (`enter 1. for area of a square or enter 2. for perimeter of a square`);
+                         
+//      if(squareuseroption === "1"){
+//                          let length = parseFloat (prompt("enter the length of the square"));
+//                          console.log(areaOfAsquare(length))
+//                          }
+//                          else if (squareuseroption === '2'){
+//                             let length = parseFloat(prompt("enter lenght of the square"));
+//                             console.log(perimeterOfAsquare(length));
+//                          } 
+                         
+
+// }
+// else if (userInput ==="2"){
+//     let rectangleUserOption = prompt(`enter 1. for Area of a rectangle or
+//                                      2. perimeter of a rectangle`);
+
+//        if (rectangleUserOption === '1'){
+//            let length = parseFloat(prompt(`enter the length of the rectangle`));
+//            let  width = parseFloat(prompt(`enter the width of the rectangle`));
+//         console.log("area of the rectangle = "+areaOfRectangle(length,width))}
+
+//         else if (rectangleUserOption === "2"){
+//             let length = parseFloat(prompt("enter the length of the rectangle"));
+//             let width = parseFloat(prompt("enter the width of the rectangle"));
+//             console.log("perimeter of the rectangle = "+ perimeterOfRectangle(length,width));}
+//     }
+
+
+
+
+
+
+
+//DOM manupulation
+
+//selection Element
+const paraTag = document.getElementById("paragraph");
+console.log(paraTag);
+
+const headingTag = document.getElementsByClassName('title');
+console.log(headingTag[1]);
+
+const btnTags = document.getElementsByTagName('button');
+console.log(btnTags);
+
+const h1Tag = document.querySelector('title');
+console.log(h1Tag);
+
+const idTag = document.querySelector('#change-button');
+console.log(idTag);
+
+const nameTag = document.querySelector('p')
+console.log(nameTag);
+
+const allH1 = document.querySelectorAll('h1')
+console.log();
+
+paraTag.textContent = "I am the new txet added to the paragraph tag";
+console.log(paraTag.textContent);
+// changing the element attribute values(class)
+console.log(paraTag.className = "paragraph12")
+// changing the element attribute values(id)
+paraTag.id = "para";
+
+console.log(paraTag);
+
+paraTag.style.backgroundColor = 'red';
+
+paraTag.style.fontStyle = 'bold';
+
+paraTag.style.border = "5px solid black"
+
+paraTag.style.borderRadius = "5px"
+
+paraTag.style.padding = "10px"
+
+// creating anf removing elements
+
+const newImageTag = document.createElement('img');
+console.log(newImageTag);
+
+    newImageTag.Id = "image"
+    newImageTag.className = 'classImage';
+    newImageTag.src = https;//kidlingoo.com/flowers-name-in-english/
+
+
+const container = document.getElementById('container')
+container.appendChild(newImageTag);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
